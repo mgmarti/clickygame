7a8ee43c29e53fb77fc94e0dd448e5b8826deb18
monica <mogimartinez@gmail.com>
2019-07-10 22:48:55 -0500

    test2

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Deploy test</h1>
+      <h1>Deploy testing</h1>
     </div>
   );
 }
