5a7d48f0ea07b42ae713f1b4f6f35e98cf344565
monica <mogimartinez@gmail.com>
2019-07-11 14:47:31 -0500

    added title component and css

---
+++ clicky-game/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+class Navbar extends React.Component {
+ render(){
+     return (
+         <div className="divider">
+             <hr />
+             <h1>clicky game</h1>
+         </div>
+     )
+ }
+}
+
+export default Navbar;
---
+++ clicky-game/src/components/Title/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+
+class Title extends React.Component {
+    render(){
+        return (
+            <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+            
+            </div>
+            </div>
+        );
+    }
+}
+
+export default Title;
