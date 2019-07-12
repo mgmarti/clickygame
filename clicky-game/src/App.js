f005229ae770fadb55e4eab12d4767e34d244a62
monica <mogimartinez@gmail.com>
2019-07-12 18:39:56 -0500

    added component to app.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './components/CharacterCard';
 import characters from "./characters.json";
@@ -6,26 +6,31 @@ import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
 
-function App() {
-  return (
-    <div>
-      <Title />
-      <Navbar />
-      <div className="wrapped">
-        <Wrapper>
-          {characters.map(character => (
-            <CharacterCard
-              id={character.id}
-              key={character.id}
-              image={character.image}
-              width={character.width}
-            />
-          ))}
-        </Wrapper>
+class App extends Component {
+  render(){
+
+    return (
+      <div>
+        <Title />
+        <Navbar />
+        <div className="wrapped">
+          <Wrapper>
+            {characters.map(character => (
+              <CharacterCard
+                id={character.id}
+                key={character.id}
+                image={character.image}
+                width={character.width}
+              />
+            ))}
+          </Wrapper>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
+
 
 
 
